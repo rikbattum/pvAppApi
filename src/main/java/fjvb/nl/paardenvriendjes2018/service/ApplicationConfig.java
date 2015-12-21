fe646c18885a7454ef318977b33292d05e8f7830
rikbattum <rikbattum@hotmail.com>
2015-12-21 16:59:01 +0100

    cors filter issue

--- src/main/java/fjvb/nl/paardenvriendjes2018/service/ApplicationConfig.java
+++ src/main/java/fjvb/nl/paardenvriendjes2018/service/ApplicationConfig.java
@@ -31,6 +31,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(fjvb.nl.paardenvriendjes2018.service.MemberFacadeREST.class);
+        resources.add(filter.corsFilter.class);
     }
 
     //Add filters for CROSS DOMAIN issues: 
